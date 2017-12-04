aea2ba335707f0eb317c858ea4eeb23b635057e5
Paul Beecroft <paul.beecrot@gmail.com>
2017-12-04 23:38:45 +0000

    fixed the filthy use of temp variable with sumBy

--- src/main/kotlin/week2/CheckSumerer.kt
+++ src/main/kotlin/week2/CheckSumerer.kt
@@ -6,24 +6,21 @@ import takeLesser
 class CheckSumerer {
 
     fun checkTheSum(spreadSheetString: String): Int
-            = wrangleSpreadsheet(spreadSheetString).map { rowCheckSum(it) }.sum()
+            = wrangleSpreadsheet(spreadSheetString).map { rowCheckSum(it) }.sumBy{ it.second - it.first }
 
     fun checkTheSum2(spreadSheetString: String): Int
-            = wrangleSpreadsheet(spreadSheetString).map { integerDivisor(it) }.sum()
+            = wrangleSpreadsheet(spreadSheetString).map { integerDivisor(it) }.sumBy{ it.first / it.second }
 
-    private fun integerDivisor(row: List<Int>): Int {
-        val matchingPair = row.flatMap { elem -> row.map { Pair(it, elem) } }
-                .filter { (first, second) -> first % second == 0 }
-                .find { (first, second) -> first != second }
-        return matchingPair!!.first / matchingPair.second
-    }
+    //TODO why can I <Pair>.sumBy() but not <Pair>.map{}.sum()
 
-    private fun rowCheckSum(row: List<Int>): Int {
-        val minAndMax = row.fold(Pair(Int.MAX_VALUE, Int.MIN_VALUE), { (first, second), elem ->
-            Pair(first.takeLesser(elem), second.takeGreater(elem))
+    private fun integerDivisor(row: List<Int>): Pair<Int, Int> = row.flatMap { elem -> row.map { Pair(it, elem) } }
+            .filter { (first, second) -> first % second == 0 }
+            .find { (first, second) -> first != second }!!
+
+    private fun rowCheckSum(row: List<Int>): Pair<Int, Int>
+            = row.fold(Pair(Int.MAX_VALUE, Int.MIN_VALUE),
+            { (first, second), elem -> Pair(first.takeLesser(elem), second.takeGreater(elem))
         })
-        return minAndMax.second - minAndMax.first
-    }
 
     private fun wrangleSpreadsheet(spreadSheetString: String): List<List<Int>>
             = spreadSheetString.split("\n")
